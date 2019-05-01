df5bbbb629c8a1136d5bfdb1cd0e0059f08e9258
Chris Kuehl <ckuehl@yelp.com>
2019-05-01 13:06:58 -0700

    v3.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pyramid-hypernova',
-    version='3.2.2',
+    version='3.3.0',
     author='Yelp, Inc.',
     author_email='opensource+pyramid-hypernova@yelp.com',
     license='MIT',
