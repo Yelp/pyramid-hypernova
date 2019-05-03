ccb11cbac9e5393b52b78bff72256b7e30d9fe03
Jason Yoon <jyoon@yelp.com>
2019-05-02 17:24:52 -0700

    Add hypernova token replacement support for JSON in py2

--- pyramid_hypernova/tweens.py
+++ pyramid_hypernova/tweens.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from json import JSONEncoder
+import json
 
 from pyramid_hypernova.batch import BatchRequest
 from pyramid_hypernova.plugins import PluginController
@@ -31,6 +32,23 @@ def hypernova_tween_factory(handler, registry):
         response = handler(request)
 
         hypernova_response = request.hypernova_batch.submit()
+
+        if request.response.content_type == 'application/json':
+            response_dict = json.loads(response.text)
+            stack = [(key, value, response_dict) for key, value in response_dict.items()]
+
+            while stack:
+                key, value, parent_dict = stack.pop()
+                if isinstance(value, dict):
+                    stack.extend([(new_key, new_value, value) for new_key, new_value in value.items()])
+                elif isinstance(value, unicode):
+                    for identifier, job_result in hypernova_response.items():
+                        token = RenderToken(identifier)
+                        value = value.replace(str(token), job_result.html)
+                    parent_dict[key] = value
+
+            response.text = json.dumps(response_dict).decode('utf-8')
+            return response
 
         for identifier, job_result in hypernova_response.items():
             token = RenderToken(identifier)
