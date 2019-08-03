54edbc3b7a9b4a903b76aae302ca86e3ac450804
Mark Larah <markl@yelp.com>
2019-08-02 17:29:06 -0700

    v5.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pyramid-hypernova',
-    version='4.0.0',
+    version='5.0.0',
     author='Yelp, Inc.',
     author_email='opensource+pyramid-hypernova@yelp.com',
     license='MIT',
