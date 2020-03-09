24f49a72f51ccae706956d7599fcd61e94c27326
Matt Mulder <muldmatt@gmail.com>
2020-03-09 16:38:46 -0700

    v7.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='pyramid-hypernova',
-    version='6.0.0',
+    version='7.0.0',
     author='Yelp, Inc.',
     author_email='opensource+pyramid-hypernova@yelp.com',
     license='MIT',
