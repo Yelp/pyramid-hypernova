d17d25aacb1ceaec64a00c6016e6fb070c07c907
shar-ajmal <sajmal@ucsd.edu>
2020-03-09 16:22:04 -0700

    Made changes for should_send_request to take in pyramid request object (#28)
    * Made changes for should_send_request to take in pyramid request object
    
    * passed in request object to every lifecycle method and rewrote tests
    
    Co-authored-by: Shaheryar Ajmal <sajmal@yelp.com>

--- tests/plugins_test.py
+++ tests/plugins_test.py
@@ -21,33 +21,41 @@ def plugin_controller(plugins):
 
 class TestPluginController(object):
     def test_get_view_data(self, plugins, plugin_controller):
+        pyramid_request = mock.Mock()
+
         data = plugin_controller.get_view_data(
             'MyComponent.js',
             {'title': 'sup'},
+            pyramid_request,
         )
 
         plugins[0].get_view_data.assert_called_once_with(
             'MyComponent.js',
             {'title': 'sup'},
+            pyramid_request,
         )
         plugins[1].get_view_data.assert_called_once_with(
             'MyComponent.js',
             plugins[0].get_view_data.return_value,
+            pyramid_request,
         )
         assert data == plugins[1].get_view_data.return_value
 
     def test_prepare_request(self, plugins, plugin_controller):
         original_jobs = [mock.Mock()]
+        pyramid_request = mock.Mock()
 
-        jobs = plugin_controller.prepare_request(original_jobs)
+        jobs = plugin_controller.prepare_request(original_jobs, pyramid_request)
 
         plugins[0].prepare_request.assert_called_once_with(
             original_jobs,
             original_jobs,
+            pyramid_request
         )
         plugins[1].prepare_request.assert_called_once_with(
             plugins[0].prepare_request.return_value,
             original_jobs,
+            pyramid_request
         )
 
         assert jobs == plugins[1].prepare_request.return_value
@@ -69,10 +77,11 @@ class TestPluginController(object):
         plugin_1_return_value,
         expected_value
     ):
+        pyramid_request = mock.Mock()
         plugins[0].should_send_request.return_value = plugin_0_return_value
         plugins[1].should_send_request.return_value = plugin_1_return_value
         jobs = mock.Mock()
-        assert plugin_controller.should_send_request(jobs) is expected_value
+        assert plugin_controller.should_send_request(jobs, pyramid_request) is expected_value
 
     def test_transform_request_headers(self, plugins, plugin_controller):
         pyramid_request = mock.Mock()
@@ -86,22 +95,26 @@ class TestPluginController(object):
 
     def test_will_send_request(self, plugins, plugin_controller):
         jobs = mock.Mock()
-        plugin_controller.will_send_request(jobs)
-        plugins[0].will_send_request.assert_called_once_with(jobs)
-        plugins[1].will_send_request.assert_called_once_with(jobs)
+        pyramid_request = mock.Mock()
+        plugin_controller.will_send_request(jobs, pyramid_request)
+        plugins[0].will_send_request.assert_called_once_with(jobs, pyramid_request)
+        plugins[1].will_send_request.assert_called_once_with(jobs, pyramid_request)
 
     def test_after_response(self, plugins, plugin_controller):
         original_response = mock.Mock()
+        pyramid_request = mock.Mock()
 
-        response = plugin_controller.after_response(original_response)
+        response = plugin_controller.after_response(original_response, pyramid_request)
 
         plugins[0].after_response.assert_called_once_with(
             original_response,
             original_response,
+            pyramid_request,
         )
         plugins[1].after_response.assert_called_once_with(
             plugins[0].after_response.return_value,
             original_response,
+            pyramid_request,
         )
 
         assert response == plugins[1].after_response.return_value
@@ -109,9 +122,10 @@ class TestPluginController(object):
     def test_on_success(self, plugins, plugin_controller):
         response = mock.Mock()
         jobs = mock.Mock()
-        plugin_controller.on_success(response, jobs)
-        plugins[0].on_success.assert_called_once_with(response, jobs)
-        plugins[1].on_success.assert_called_once_with(response, jobs)
+        pyramid_request = mock.Mock()
+        plugin_controller.on_success(response, jobs, pyramid_request)
+        plugins[0].on_success.assert_called_once_with(response, jobs, pyramid_request)
+        plugins[1].on_success.assert_called_once_with(response, jobs, pyramid_request)
 
     def test_on_error(self, plugins, plugin_controller):
         err = mock.Mock()
@@ -129,15 +143,18 @@ class TestBasePlugin(object):
     def test_get_view_data(self):
         plugin = BasePlugin()
         data = mock.sentinel.data
-        assert plugin.get_view_data('MyComponent.js', data) == data
+        pyramid_request = mock.Mock()
+        assert plugin.get_view_data('MyComponent.js', data, pyramid_request) == data
 
     def test_prepare_request(self):
         plugin = BasePlugin()
         current_jobs = mock.sentinel.current_jobs
         original_jobs = mock.sentinel.original_jobs
+        pyramid_request = mock.Mock()
         assert plugin.prepare_request(
             current_jobs,
             original_jobs,
+            pyramid_request
         ) == current_jobs
 
     def test_transform_request_headers(self):
@@ -146,15 +163,18 @@ class TestBasePlugin(object):
         assert plugin.transform_request_headers({'foo': 'bar'}, pyramid_request) == {'foo': 'bar'}
 
     def test_should_send_request(self):
+        pyramid_request = mock.Mock()
         plugin = BasePlugin()
         jobs = mock.sentinel.jobs
-        assert plugin.should_send_request(jobs)
+        assert plugin.should_send_request(jobs, pyramid_request)
 
     def test_after_response(self):
         plugin = BasePlugin()
         current_response = mock.sentinel.current_response
         original_response = mock.sentinel.original_response
+        pyramid_request = mock.Mock()
         assert plugin.after_response(
             current_response,
             original_response,
+            pyramid_request,
         ) == current_response
