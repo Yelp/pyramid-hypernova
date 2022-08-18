6508b9332b2617fb4b7c7e0ea08ac3fce3199aaf
Joe Bateson <jbateson@yelp.com>
2022-08-17 19:17:36 -0700

    Update docstring referencing py2

--- pyramid_hypernova/token_replacement.py
+++ pyramid_hypernova/token_replacement.py
@@ -7,7 +7,7 @@ from pyramid_hypernova.rendering import RenderToken
 def hypernova_token_replacement(hypernova_batch):
     """A context manager that performs hypernova token replacement in a batch.
     Write the content you wish to modify in body['content'] where body is the
-    yielded dict. Written content must be of unicode type in py2 and str in py3.
+    yielded dict. Written content must be str in py3.
 
     Example usage:
         with hypernova_token_replacement(hypernova_batch) as body:
