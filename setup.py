033e73979e22cb984f4e24d935b89f5782d5a4d3
Preksha Kashyap <pkashyap@yelp.com>
2023-03-17 09:33:27 -0700

    Release v9.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyramid-hypernova',
-    version='9.3.0',
+    version='9.3.1',
     author='Yelp, Inc.',
     author_email='opensource+pyramid-hypernova@yelp.com',
     license='MIT',
