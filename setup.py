293199bcdec1f9ac2af2b77181ac48cc5f0589ae
J.P. McDevitt <jpmcd@yelp.com>
2025-06-25 15:26:48 -0700

    Release v10.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyramid-hypernova',
-    version='10.0.0',
+    version='10.0.1',
     author='Yelp, Inc.',
     author_email='opensource+pyramid-hypernova@yelp.com',
     license='MIT',
